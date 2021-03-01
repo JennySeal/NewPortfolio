5ea3a2d12807f990911d8e4b2ecc33495384189e
JennySeal <78556111+JennySeal@users.noreply.github.com>
2021-03-01 16:44:49 +0000

    Laptop size website ready

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const changeDescriptor = () => {
 }
 
 setInterval(changeDescriptor, 2000);
+
+function printLetterByLetter(destination, message, speed){
+    let i = 0;
+    let interval = setInterval(function(){
+        document.getElementById(destination).innerHTML += message.charAt(i);
+        i++;
+        if (i > message.length){
+                document.getElementById(destination).innerHTML = message.charAt(0);
+            i=1;
+        }
+    }, speed);
+} 
+ 
+printLetterByLetter("scrollScript", "<I would love to hear from you.>   ", 300);
